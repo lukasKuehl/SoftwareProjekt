8490e345f3503f8b6354421f81890bd11247eb89
Ramona <Ramona@SURFACE-MONA.dhcp.fh-bielefeld.de>
2018-01-02 14:56:05 +0100

    tetet

--- view/KrankmeldungLoeschenView.java
+++ view/KrankmeldungLoeschenView.java
@@ -1,6 +1,7 @@
 package view;
 
 
+
 import java.awt.*;
 
 import javax.swing.*;
--- view/TerminLoeschenView.java
+++ view/TerminLoeschenView.java
@@ -1,6 +1,7 @@
 package view;
 
 
+
 import java.awt.BorderLayout;
 import java.awt.Color;
 import java.awt.EventQueue;
